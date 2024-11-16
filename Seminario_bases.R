e31e0b1c12a08bb95de76b1c4c2b72630adec885
ayatoussi <ayatoussi47@gmail.com>
2024-11-16 15:20:34 +0100

    ficheros y cargando tablas

--- Seminario_bases.R
+++ Seminario_bases.R
@@ -15,3 +15,19 @@ con <- dbConnect(RPostgres::Postgres(),
                  port = 5432,                  
                  user = "postgres",          
                  password = "postgres")
+
+#Cargar tabla pacientes
+
+tabla_paciente<-read.csv("TABLAS/pacientes.csv")
+
+#Visualizar tabla pacientes 
+tabla_paciente
+
+#Cargar y Visualizar tabla diagnosticos
+tabla_diagnosticos<-read.csv("TABLAS/diagnosticos.csv")
+tabla_diagnosticos
+
+#Cargar y visualizar tablas tratamientos
+
+tabla_tto<-read.csv("TABLAS/diagnosticos.csv")
+tabla_tto
