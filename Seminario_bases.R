a9d105c970527a156da9a1c87b0128824d1464f7
ayatoussi <ayatoussi47@gmail.com>
2024-11-19 22:31:34 +0100

    primera tabla

--- Seminario_bases.R
+++ Seminario_bases.R
@@ -17,8 +17,31 @@ con <- dbConnect(RPostgres::Postgres(),
                  password = "postgres")
 
 
+dbListTables(con)
 #CREANDO LA TABLA DE TRATAMIENTOS : 
 
+tto<-read.csv("TABLAS/tratamientos.csv")
+
+tto
+
+tratamientos<-dbExecute(con, 
+          "
+          create table tratamientos (
+          id_tratamiento SERIAL PRIMARY KEY , 
+          id_paciente varchar(50),
+          tipo_tratamiento varchar(50),
+          detalle varchar(50),
+          duracion_meses smallint check (duracion_meses >=0 and duracion_meses<=24) , 
+          frecuencia varchar(30) check (frecuencia in ('Diario','Única','Semanal','Mensual')) , 
+          resultado_esperado varchar(70) not null 
+          )" )
+
+
+
+#CREANDO LA TABLA DE PACIENTES :
+
+
+#CREANDO LA TABLA DE DIAGNOSTICOS : 
 
 
 
